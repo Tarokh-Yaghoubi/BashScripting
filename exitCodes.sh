b28c77f7bf6ca4244a814b4807fed6a770405766
Tarokh Yaghoubi <tarokhgit@gmail.com>
2025-03-05 23:40:58 +0330

    exit codes in BASH scripting, [DONE]

--- exitCodes.sh
+++ exitCodes.sh
@@ -2,6 +2,8 @@
 
 directory=/etc
 package=neofetch
+anotherDirectory=/notexist
+
 sudo apt install $package
 
 if [ "$?" -eq 0 ]; then
@@ -18,3 +20,24 @@ then
 else
 	echo "the dir does not exist"
 fi	
+
+if [ -d $anotherDirectory ]; then
+	echo "Here the exit code is $?"
+	echo "The $anotherDirectory exists"
+else
+	echo "Here the exit code is $?"
+	echo "The $anotherDirectory does not exist"
+	echo "But here it is -> $?" 
+fi
+
+echo "--------------LETS DO SOMETHING ELSE---------------"
+if [ -d $anotherDirectory ]
+then
+	echo "The $directory does  exist"
+	exit $?
+
+else
+	echo "The exit code is now $?"
+	exit $?
+fi
+
